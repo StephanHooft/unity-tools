30fb2a0ddc8ea5b63516eccb73d530d98b86e850
Stephan Hooft <59238657+StephanHooft@users.noreply.github.com>
2022-01-31 04:01:25 +0100

    Introduced LayerMaskExtensions

---
+++ Runtime/Extensions/Unity/LayerMaskExtensions.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+namespace StephanHooft.Extensions
+{
+    public static class LayerMaskExtensions
+    {
+        /// <summary>
+        /// Checks whether a <see cref="GameObject"/>'s layer is included in the <see cref="LayerMask"/>.
+        /// </summary>
+        /// <param name="gameObject">The <see cref="GameObject"/> to check against.</param>
+        /// <returns><see cref="true"/> if the <see cref="LayerMask"/> contains the <see cref="GameObject"/>'s
+        /// layer.</returns>
+        private static bool Contains(this LayerMask mask, GameObject gameObject)
+        {
+            var objLayerMask = 1 << gameObject.layer;
+            return
+                (mask.value & objLayerMask) > 0;
+        }
+
+        /// <summary>
+        /// Checks whether a <see cref="Component"/>'s layer is included in the <see cref="LayerMask"/>.
+        /// </summary>
+        /// <param name="component">The <see cref="Component"/> to check against.</param>
+        /// <returns><see cref="true"/> if the <see cref="LayerMask"/> contains the <see cref="Component"/>'s
+        /// layer.</returns>
+        private static bool Contains(this LayerMask mask, Component component)
+        {
+            var objLayerMask = 1 << component.gameObject.layer;
+            return
+                (mask.value & objLayerMask) > 0;
+        }
+
+        /// <summary>
+        /// Checks whether an <see cref="int"/> layer is included in the <see cref="LayerMask"/>.
+        /// </summary>
+        /// <param name="layer">The layer <see cref="int"/> to check against.</param>
+        /// <returns><see cref="true"/> if the <see cref="LayerMask"/> contains the <see cref="int"/>
+        /// layer.</returns>
+        private static bool Contains(this LayerMask mask, int layer)
+        {
+            var objLayerMask = 1 << layer;
+            return
+                (mask.value & objLayerMask) > 0;
+        }
+        
+        /// <summary>
+        /// Returns <see cref="true"/> if the <see cref="LayerMask"/> is empty.
+        /// </summary>
+        /// <returns><see cref="true"/> if the <see cref="LayerMask"/> has zero layers included.</returns>
+        private static bool IsEmpty(this LayerMask mask)
+        {
+            return mask.value == 0;
+        }
+    }
+}
