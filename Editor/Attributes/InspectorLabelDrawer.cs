3978349d1ee8467126a7c4e74e04e5cd6a99e25c
Stephan Hooft <59238657+StephanHooft@users.noreply.github.com>
2022-02-26 14:58:16 +0100

    Renamed InspectorLabel attribute

---
+++ Editor/Attributes/InspectorLabelDrawer.cs
@@ -0,0 +1,25 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace StephanHooft.Attributes.EditorScripts
+{
+    /// <summary>
+    /// A <see cref="CustomPropertyDrawer"/> for the <see cref="InspectorLabel"/>.
+    /// </summary>
+    [CustomPropertyDrawer(typeof(InspectorLabel))]
+    public class InspectorLabelDrawer : PropertyDrawer
+    {
+        #region PropertyDrawer Implementation
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            InspectorLabel[] overriddenLabel = (InspectorLabel[])fieldInfo.GetCustomAttributes(typeof(InspectorLabel), true);
+            var labelText = overriddenLabel[0] != null ? new GUIContent(overriddenLabel[0].Label) : label;
+            EditorGUI.PropertyField(position, property, labelText);
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        #endregion
+    }
+}
--- Runtime/Attributes/InspectorLabel.cs
+++ Runtime/Attributes/InspectorLabel.cs
@@ -4,13 +4,15 @@ namespace StephanHooft.Attributes
 {
     /// <summary>
     /// Class to override the inspector label of a field.
-    /// <remarks><para>Borrowed from: https://answers.unity.com/questions/1005277/can-i-change-variable-name-on-inspector.html .</para></remarks>
     /// </summary>
-    public class LabelOverride : PropertyAttribute
+    public class InspectorLabel : PropertyAttribute
     {
         #region Fields
 
-        public string label;
+        /// <summary>
+        /// The label <see cref="string"/> to show in the inspector.
+        /// </summary>
+        public string Label { get; private set; }
 
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         #endregion
@@ -19,14 +21,15 @@ namespace StephanHooft.Attributes
 
         /// <summary>
         /// Overrides the inspector label of a <see cref="string"/> field.
+        /// <para>Doesn't work on arrays, lists and other collections.</para>
         /// </summary>
         /// <param name="label">The label <see cref="string"/> to show in the inspector.</param>
-        public LabelOverride(string label)
+        public InspectorLabel(string label)
         {
             if (string.IsNullOrEmpty(label))
                 throw
                     new System.ArgumentException("label");
-            this.label = label;
+            Label = label;
         }
 
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
