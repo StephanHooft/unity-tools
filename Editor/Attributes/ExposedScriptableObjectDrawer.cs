528ad5e9066d5f20ff8c403fbdfa379a40e63466
Stephan Hooft <59238657+StephanHooft@users.noreply.github.com>
2022-07-31 22:57:19 +0200

    Don't try to draw expanded scriptable object if no object reference is set

--- Editor/Attributes/ExposedScriptableObjectDrawer.cs
+++ Editor/Attributes/ExposedScriptableObjectDrawer.cs
@@ -36,14 +36,16 @@ namespace StephanHooft.Attributes.EditorScripts
             {
                 EditorGUI.PropertyField(position, property, label, true);
                 if (property.objectReferenceValue != null)
-                    property.isExpanded = EditorGUI.Foldout(position, property.isExpanded, GUIContent.none);
-                if (property.isExpanded)
                 {
-                    EditorGUI.indentLevel++;
-                    if (!editor)
-                        Editor.CreateCachedEditor(property.objectReferenceValue, null, ref editor);
-                    editor.OnInspectorGUI();
-                    EditorGUI.indentLevel--;
+                    property.isExpanded = EditorGUI.Foldout(position, property.isExpanded, GUIContent.none);
+                    if (property.isExpanded)
+                    {
+                        EditorGUI.indentLevel++;
+                        if (!editor)
+                            Editor.CreateCachedEditor(property.objectReferenceValue, null, ref editor);
+                        editor.OnInspectorGUI();
+                        EditorGUI.indentLevel--;
+                    }
                 }
             }
         }
